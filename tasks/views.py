1f817f2154dc8ddec567e43a41ef16a434d2dc29
AmiNul030 <aminulhaquetanim30@gmail.com>
2026-03-04 03:05:35 +0600

    view complete

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ def task_list(request):
     tasks = Task.objects.all().order_by('completed', '-id')
     return render(request,"tasks/task_list.html",{"tasks": tasks})
 
+
 def task_add(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
@@ -14,8 +15,8 @@ def task_add(request):
             return redirect("task_list")
     else:
         form = TaskForm()
-
     return render(request, "tasks/task_form.html", {"form": form, "title": "Add New Task"})
+
 
 
 def task_edit(request, pk):
@@ -31,4 +32,18 @@ def task_edit(request, pk):
 
     return render(request, "tasks/task_form.html", {"form": form, "title": "Edit Task"})
 
-#temp
+
+def task_delete(request, pk):
+    task= get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect("task_list")
+    return render(request, "tasks/task_confirm_delete.html", {"task": task})
+
+def task_toggle(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.completed = not task.completed
+        task.save()
+    
+    return redirect("task_list")
