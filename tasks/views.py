d80484eb83c1eeb87aae4bfc67ed40cf9300c9d7
AmiNul030 <aminulhaquetanim30@gmail.com>
2026-03-04 02:13:45 +0600

    Initial commit: Django task manager

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all().order_by('completed', '-id')
+    return render(request,"tasks/task_list.html",{"tasks": tasks})
+
+def task_add(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm()
+
+    return render(request, "tasks/task_form.html", {"form": form, "title": "Add New Task"})
